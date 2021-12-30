bd1c79e4b4ce58dd58208303a18a2a77c1ed07ea
rajeshSigmoid <rajeshk@sigmoidanalytics.com>
2021-12-30 12:55:40 +0530

    removed connection-types

--- firebolt_provider/__init__.py
+++ firebolt_provider/__init__.py
@@ -4,7 +4,6 @@ def get_provider_info():
         "name": "Firebolt Provider",
         "description": "A Firebolt provider for Apache Airflow.",
         "hook-class-names": ["firebolt_provider.hooks.firebolt.FireboltHook"],
-        "connection-types": ["firebolt_provider.hooks.firebolt.FireboltHook"],
         "extra-links": ["firebolt_provider.operators.firebolt.RegistryLink"],
         "versions": ["0.0.1"]
         }
