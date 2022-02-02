9d4dcaad9b8344cd0cadaeec5f80ac60562f1bed
Petro Tiurin <93913847+ptiurin@users.noreply.github.com>
2022-02-02 14:42:31 +0000

    refactor: Remove versions from get_provider_info

--- firebolt_provider/__init__.py
+++ firebolt_provider/__init__.py
@@ -8,5 +8,4 @@ def get_provider_info() -> Dict[str, Any]:
         "description": "A Firebolt provider for Apache Airflow.",
         "hook-class-names": ["firebolt_provider.hooks.firebolt.FireboltHook"],
         "extra-links": ["firebolt_provider.operators.firebolt.RegistryLink"],
-        "versions": ["0.0.1"],
     }
