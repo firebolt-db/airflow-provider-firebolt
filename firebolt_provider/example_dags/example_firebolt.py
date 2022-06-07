df866328935bfb6d4bef05a9905c56b8e16bf2ca
rajeshmauryasde <rajeshk@sigmoidanalytics.com>
2022-06-07 16:19:52 +0530

    fix: removed dependency on pre-existing tables. (#19)
    * refactored example_dag
    
    * replaced single quotes with double quotes
    
    * refactored code to pass mypy test
    
    * resolved typing issues
    
    * resolved typing issues
    
    * feat:add feat title for the new PR
    
    Co-authored-by: Yury Dzerin <yury@firebolt.io>

--- firebolt_provider/example_dags/example_firebolt.py
+++ firebolt_provider/example_dags/example_firebolt.py
@@ -29,29 +29,23 @@ from firebolt_provider.operators.firebolt import (
 )
 
 FIREBOLT_CONN_ID = "firebolt_conn_id"
-FIREBOLT_SAMPLE_TABLE = "order_details"
-FIREBOLT_DATABASE = "yury_integration_tests_ingest"
-FIREBOLT_ENGINE = "yury_integration_tests_ingest_Ingest2"
+FIREBOLT_SAMPLE_TABLE = "sample_table"
+FIREBOLT_DATABASE = "sample_database"
+FIREBOLT_ENGINE = "sample_engine"
 
 # SQL commands
-SELECT_STATEMENT_SQL_STRING = f"SELECT * FROM {FIREBOLT_SAMPLE_TABLE} LIMIT 1;"
-SQL_INSERT_STATEMENT = (
-    f"INSERT INTO {FIREBOLT_SAMPLE_TABLE} values "
-    f"(?,'Oil - Shortening - All - Purpose',"
-    f"6928105225,5,4784.12,'2019-06-05','2019-06-05 04:02:08',1); "
-)
-SQL_LIST = [
-    f"SELECT * FROM {FIREBOLT_SAMPLE_TABLE} LIMIT 2;",
-    "select * from lineitem limit 1;",
-]
-SQL_CREATE_DATABASE_STATEMENT = "CREATE DATABASE IF NOT EXISTS my_db1;"
-SQL_DROP_DATABASE_STATEMENT = "DROP DATABASE IF EXISTS my_db1;"
 SQL_CREATE_TABLE_STATEMENT = (
-    "CREATE FACT TABLE IF NOT EXISTS users12 "
-    "(id INT, name String, last_login DateTime, password String)"
-    " PRIMARY INDEX id;"
+    f"CREATE FACT TABLE IF NOT EXISTS {FIREBOLT_SAMPLE_TABLE} "
+    f"(id INT, name String)"
+    f" PRIMARY INDEX id;"
 )
-SQL_DROP_TABLE_STATEMENT = "DROP TABLE IF EXISTS users12;"
+SQL_INSERT_STATEMENT = f"INSERT INTO {FIREBOLT_SAMPLE_TABLE} values (%(id)s, 'name');"
+SQL_LIST = [SQL_INSERT_STATEMENT % {"id": n} for n in range(0, 10)]
+SELECT_STATEMENT_SQL_STRING = f"SELECT * FROM {FIREBOLT_SAMPLE_TABLE} LIMIT 10;"
+SQL_DROP_TABLE_STATEMENT = f"DROP TABLE IF EXISTS {FIREBOLT_SAMPLE_TABLE};"
+
+SQL_CREATE_DATABASE_STATEMENT = f"CREATE DATABASE IF NOT EXISTS {FIREBOLT_DATABASE};"
+SQL_DROP_DATABASE_STATEMENT = f"DROP DATABASE IF EXISTS {FIREBOLT_DATABASE};"
 
 with DAG(
     "example_firebolt",
@@ -60,14 +54,17 @@ with DAG(
     tags=["example"],
     catchup=False,
 ) as dag:
-    firebolt_start_engine = FireboltStartEngineOperator(
-        task_id="firebolt_start_engine",
-        engine_name=FIREBOLT_ENGINE,
+    firebolt_start_engine = FireboltStartEngineOperator(engine_name=FIREBOLT_ENGINE)
+    firebolt_stop_engine = FireboltStopEngineOperator(engine_name=FIREBOLT_ENGINE)
+
+    firebolt_op_sql_create_table = FireboltOperator(
+        task_id="firebolt_op_sql_create_table",
+        sql=SQL_CREATE_TABLE_STATEMENT,
     )
 
-    firebolt_op_sql_str = FireboltOperator(
-        task_id="firebolt_op_sql_str",
-        sql=SELECT_STATEMENT_SQL_STRING,
+    firebolt_op_sql_list = FireboltOperator(
+        task_id="firebolt_op_sql_list",
+        sql=SQL_LIST,
     )
 
     firebolt_op_with_params = FireboltOperator(
@@ -76,9 +73,14 @@ with DAG(
         parameters=[56],
     )
 
-    firebolt_op_sql_list = FireboltOperator(
-        task_id="firebolt_op_sql_list",
-        sql=SQL_LIST,
+    firebolt_op_sql_str = FireboltOperator(
+        task_id="firebolt_op_sql_str",
+        sql=SELECT_STATEMENT_SQL_STRING,
+    )
+
+    firebolt_op_sql_drop_table = FireboltOperator(
+        task_id="firebolt_op_sql_drop_table",
+        sql=SQL_DROP_TABLE_STATEMENT,
     )
 
     firebolt_op_sql_create_db = FireboltOperator(
@@ -91,29 +93,14 @@ with DAG(
         sql=SQL_DROP_DATABASE_STATEMENT,
     )
 
-    firebolt_op_sql_create_table = FireboltOperator(
-        task_id="firebolt_op_sql_create_table",
-        sql=SQL_CREATE_TABLE_STATEMENT,
-    )
-
-    firebolt_op_sql_drop_table = FireboltOperator(
-        task_id="firebolt_op_sql_drop_table",
-        sql=SQL_DROP_TABLE_STATEMENT,
-    )
-
-    firebolt_stop_engine = FireboltStopEngineOperator(
-        task_id="firebolt_stop_engine",
-        engine_name=FIREBOLT_ENGINE,
-    )
-
     (
         firebolt_start_engine
-        >> firebolt_op_sql_str
-        >> firebolt_op_with_params
+        >> firebolt_op_sql_create_table
         >> firebolt_op_sql_list
+        >> firebolt_op_with_params
+        >> firebolt_op_sql_str
+        >> firebolt_op_sql_drop_table
         >> firebolt_op_sql_create_db
         >> firebolt_op_sql_drop_db
-        >> firebolt_op_sql_create_table
-        >> firebolt_op_sql_drop_table
         >> firebolt_stop_engine
     )
